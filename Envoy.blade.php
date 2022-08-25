baccf28fe7514b2574e9e37d6e9895f2eb3492bf
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-08-25 15:47:36 +0300

    Fix envoy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,6 +3,8 @@
 @task('deploy', ['on' => ['localhost']])
     git fetch
     git reset --hard origin/main
-    php artisan migrate --force
-    npm run build
+    /usr/bin/php8.0 artisan migrate --force
+    /usr/bin/php8.0 artisan ziggy:generate
+    nvm exec stable npm run build
+    chown -R www-data:www-data /var/www/prinbanat.ngo
 @endtask
