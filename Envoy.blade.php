6c23d3d6646a689ce71d3f40d31e2de70f6f0328
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-18 21:42:21 +0300

    Run installer

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,6 +6,7 @@
     /usr/bin/php8.0 /usr/bin/composer install -o -vv
     /usr/bin/php8.0 artisan migrate --force
     /usr/bin/php8.0 artisan ziggy:generate
+    npm install
     npm run build
     chown -R www-data:www-data /var/www/prinbanat.ngo
 @endtask
