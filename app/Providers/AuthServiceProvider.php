3de2ccb7f8bcf14ae76559e7e58e4109bd76ce71
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-20 16:01:53 +0300

    Fix email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(static function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject(__('Why Culture Matters? // Verify Email Address'))
+                ->line(__("In order to complete your registration at Why Culture Matters? International Symposium, we need to verify your email address. Please click the button below."))
+                ->action(__('Verify Email Address'), $url);
+        });
     }
 }
