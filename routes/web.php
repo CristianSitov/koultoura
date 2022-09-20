86ef8a2ce974356fa0caaaec4e2161a719fc12a8
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-20 03:39:14 +0300

    Restore?

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,22 @@ use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
 Route::get('/', static function () {
     return view('welcome');
 });
+
 Route::get('/wcm', [FrontController::class, 'index'])
     ->name('home');
 
-Route::get('/{locale}', [FrontController::class, 'locale'])
+Route::get('/{locale}', static function ($locale = null) {
+    if (isset($locale) && in_array($locale, config('translatable.locales'), true)) {
+        app()->setLocale($locale);
+        session()->put('locale', $locale);
+    }
+
+    return redirect()->back();
+})
     ->where('locale', 'en|ro');
 
 Route::middleware([
@@ -18,6 +27,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', [FrontController::class, 'dashboard'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 });
 
