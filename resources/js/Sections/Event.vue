9a75dd32616e7d08d044adaeebcd25bd1bf45e91
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-21 00:31:40 +0300

    Bolds

--- resources/js/Sections/Event.vue
+++ resources/js/Sections/Event.vue
@@ -4,7 +4,7 @@
             <section class="max-w-7xl mx-auto px-4 pt-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28">
                 <div class="flex flex-col md:flex-row md:space-x-12">
                     <h1 class="md:basis-1/3 text-4xl sm:text-5xl lg:text-7xl p-5 font-bold uppercase sm:mb-10 wcm">why culture matters?</h1>
-                    <p class="md:basis-2/3 text sm:text-xl lg:text-3xl p-5 pb-10 md:pb-20 lg:p-20 about font-bold leading-snug"
+                    <p class="md:basis-2/3 text sm:text-xl lg:text-3xl p-5 pb-10 md:pb-20 lg:p-20 about font-normal leading-snug"
                         v-html="$t('Event')"></p>
                 </div>
             </section>
