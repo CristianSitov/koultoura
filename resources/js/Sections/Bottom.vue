8d9f94306a26f1102204f8b37dc7d90ab1d0323e
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-20 22:08:38 +0300

    Update

--- resources/js/Sections/Bottom.vue
+++ resources/js/Sections/Bottom.vue
@@ -3,11 +3,6 @@
         <footer class="my-20 p-4 bg-white md:p-8 lg:p-10">
             <div class="mx-auto max-w-screen-xl text-center">
                 <a href="/public" class="flex justify-center items-center text-5xl font-semibold text-red-600">why culture matters</a>
-                <ul class="flex flex-wrap justify-center items-center mt-16 mb-28 text-gray-900">
-                    <li v-for="item in $page.props.navigation" :key="item.name">
-                        <a :href="item.href" class="mr-4 hover:underline md:mx-6 ">{{ $t(item.name) }}</a>
-                    </li>
-                </ul>
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <span class="text-sm text-gray-500 sm:text-center">© 2022 <strong><a href="//prinbanat.ngo" class="hover:underline">Prin Banat NGO</a></strong>. {{ $t('All Rights Reserved.') }}</span>
                     <div class="mt-4 text-sm sm:justify-center sm:mt-0 text-gray-500" v-html="$t('Made with ♡')"></div>
