8ec0a8feb16fca1202bfb3828535bda4cee88b2b
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-20 22:31:06 +0300

    PUBLISH 🚀

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('/', [FrontController::class, 'splash']);
+//Route::get('/', [FrontController::class, 'splash']);
 
-Route::get('/wcm', [FrontController::class, 'index'])
+Route::get('/', [FrontController::class, 'index'])
     ->name('home');
 
 Route::get('/registration', [FrontController::class, 'registration'])
