fb8285d59c24eeaf9691514b88cf810171e73a45
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-21 12:23:21 +0300

    asaaaa

--- resources/js/Sections/HomeHero.vue
+++ resources/js/Sections/HomeHero.vue
@@ -6,7 +6,7 @@ import {InertiaLink} from '@inertiajs/inertia-vue3'
 <template>
     <div class="snap-start scroll-m-0 snap-both">
         <div class="bg-white bg-cover bg-no-repeat"
-             style="background: url(assets/images/propunere_background_site_wcm_2.png) no-repeat;">
+             style="background: url(assets/images/wall_1.jpg) no-repeat;">
             <div class="w-full mx-auto">
                 <div class="">
                     <div class="max-w-7xl mx-auto my-auto px-4 py-44">
