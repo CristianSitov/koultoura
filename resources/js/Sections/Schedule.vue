7cf59abe259e2b085c64c1230d80c14f440bb4d0
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2022-09-29 20:00:49 +0300

    Make it clearer

--- resources/js/Sections/Schedule.vue
+++ resources/js/Sections/Schedule.vue
@@ -40,12 +40,15 @@ const props = defineProps({
                             <div class="md:w-3/4 mx-auto">
                                 <h3 class="text-2xl font-bold uppercase pt-6 md:pt-12">{{ $page.props.days[index].title }}</h3>
                                 <p class="row-span-2 py-3" v-html="$page.props.days[index].description"></p>
-                                <p><span class="inline-flex leading-6 lg:leading-10 mb-4"><LocationMarkerIcon
-                                    class="h-6 md:h-9 sm:h-7 w-7 md:w-9 sm:w-7 mr-4" aria-hidden="true"/>{{ $page.props.days[index].location }}</span></p>
-                                <p><span class="inline-flex leading-6 lg:leading-10 mb-4"><UserIcon
-                                    class="h-5 md:h-9 sm:h-7 w-5 md:w-9 sm:w-7 mr-4" aria-hidden="true"/>{{ $t('Host Moderator') }}: {{ $page.props.days[index].host?.full_name || 'TBA'}}</span></p>
-                                <p><span class="inline-flex leading-6 lg:leading-10 mb-4"><UserIcon
-                                    class="h-5 md:h-9 sm:h-7 w-5 md:w-9 sm:w-7 mr-4" aria-hidden="true"/>{{ $t('Moderator(s)') }}: {{ $page.props.days[index].moderators.map((m) => m.full_name).join(", ") || 'TBA'}}</span></p>
+                                <p class="flex flex-row mb-3"><span class="leading-6 lg:leading-10 mb-2"><LocationMarkerIcon
+                                    class="h-6 md:h-9 sm:h-7 w-7 md:w-9 sm:w-7 mr-2" aria-hidden="true"/></span>
+                                    <span class="inline-flex font-bold">{{ $page.props.days[index].location }}</span></p>
+                                <p class="flex flex-row mb-3"><span class="leading-6 lg:leading-10 mb-2"><UserIcon
+                                    class="h-5 md:h-9 sm:h-7 w-7 md:w-9 sm:w-7 mr-2" aria-hidden="true"/></span>
+                                    <span class="inline-flex">{{ $t('Host Moderator') }}:&nbsp;<span class="font-bold">{{ $page.props.days[index].host?.full_name || 'TBA'}}</span></span></p>
+                                <p class="flex flex-row mb-3"><span class="leading-6 lg:leading-10 mb-2"><UserIcon
+                                    class="h-5 md:h-9 sm:h-7 w-7 md:w-9 sm:w-7 mr-2" aria-hidden="true"/></span>
+                                    <span class="inline-flex">{{ $t('Moderator(s)') }}:&nbsp;<span class="font-bold">{{ $page.props.days[index].moderators.map((m) => m.full_name).join(", ") || 'TBA'}}</span></span></p>
                             </div>
                             <div class="container md:px-5 py-10 md:py-24 mx-auto flex flex-wrap">
                                 <div v-for="presentation in day" :key="presentation.id" class="flex relative pt-5 mt-3 md:pt-10 md:pb-20 sm:items-center mx-auto w-full">
